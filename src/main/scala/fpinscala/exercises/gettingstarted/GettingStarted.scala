a1bad0134124bfe9f0b2ab05f4d8e825a99cab7f
Marco Mengoli <marco.mengoli4@gmail.com>
2023-01-14 01:05:30 +0100

    Implemented exercises chapter1 getting started

--- src/main/scala/fpinscala/exercises/gettingstarted/GettingStarted.scala
+++ src/main/scala/fpinscala/exercises/gettingstarted/GettingStarted.scala
@@ -33,7 +33,17 @@ object MyProgram:
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(n:Int, cur:Int, prev:Int):Int = {
+      n match{
+        case 0 => prev
+        case i => go(i-1, cur+prev, cur)
+      }
+    }
+
+    go(n, 1, 0)
+  }
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) =
@@ -121,8 +131,16 @@ object PolymorphicFunctions:
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(i: Int): Boolean = {
+      if i >= as.length - 1 then true
+      else if gt(as(i), as(i+1)) then false
+      else go(i+1)
+    }
 
+    go(0)
+  }
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
 
@@ -134,13 +152,15 @@ object PolymorphicFunctions:
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+    a => b => f(a, b)
+    // (a: A) => ((b: B) => f(a, b))  types can be omitted
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+    (a, b) => f(a)(b)
+    //(a: A, b: B) => f(a)(b)  types can be omitted
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -155,5 +175,6 @@ object PolymorphicFunctions:
   // Exercise 5: Implement `compose`
 
   def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+    a => f(g(a))
+    // (a: A) => f(g(a))  types can be omitted
 
